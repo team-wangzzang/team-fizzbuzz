dd88ad30a58d1f2d1c4599e0d63cc8a89310ef0d
ulgoon <ulgoon89@gmail.com>
2023-09-18 16:17:35 +0900

    feat: Print Fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,9 @@
-print('hello')
+for i in range(1, 20+1):
+    if i % 15 == 0:
+        print('fizzbuzz')
+    elif i % 5 == 0:
+        print('buzz')
+    elif i % 3 == 0:
+        print('fizz')
+    else:
+        print(i)
